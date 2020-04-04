6eb1ded7c54e3c6027c8c84155e3f85b5fb65df7
Richard LaConte <38797549+pakjones@users.noreply.github.com>
2020-04-04 14:59:37 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,141 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+app.get('/js', (req, res) => {
+    res.sendFile(__dirname + '/public/main.js');
+})
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/public/main.css');
+})
+
+let game = {
+    currentUsers: [],
+    objectives: [
+        { left: Math.floor(Math.random() * 400), top: Math.floor(Math.random() * 400) }
+    ]
+}
+
+const newObj = () => {
+    game.objectives[0].left = Math.floor(Math.random() * 400);
+    game.objectives[0].top = Math.floor(Math.random() * 400)
+}
+
+// CURRENT USERS HANDLING
+const createUser = (id) => {
+    return {
+        id,
+        moveUp() {
+            this.top -= this.speed;
+            return;
+        },
+        moveDown() {
+            this.top += this.speed;
+            return;
+        },
+        moveRight() {
+            this.left += this.speed;
+            return;
+        },
+        moveLeft() {
+            this.left -= this.speed;
+            return;
+        },
+        levelUp() {
+            this.size += 5;
+            this.speed += 1;
+        },
+        top: Math.floor(Math.random() * 400),
+        left: Math.floor(Math.random() * 300),
+        size: 10,
+        speed: 10
+    }
+}
+const addUser = (id) => {
+    game.currentUsers.push(createUser(id));
+}
+const getUserById = (id) => {
+    for (user in game.currentUsers) {
+        if (game.currentUsers[user].id === id) {
+            return game.currentUsers[user];
+        }
+    }
+}
+const removeUser = (id) => {
+    for (let i = 0; i < game.currentUsers.length; i++) {
+        if (game.currentUsers[i].id === id) {
+            game.currentUsers.splice(i, i + 1);
+        }
+    }
+}
+
+const checkUserObj = (user) => {
+    let objCenterLeft = game.objectives[0].left + 10;
+    let objCenterTop = game.objectives[0].top + 10;
+    let userCenterLeft = user.left + (user.size / 2);
+    let userCenterTop = user.top + (user.size / 2);
+
+
+    let xDiff;
+    let yDiff;
+    if (objCenterLeft > userCenterLeft) {
+        xDiff = objCenterLeft - userCenterLeft;
+    } else {
+        xDiff = userCenterLeft - objCenterLeft;
+    }
+    if (objCenterTop > userCenterTop) {
+        yDiff = objCenterTop - userCenterTop;
+    } else {
+        yDiff = userCenterTop - objCenterTop;
+    }
+
+    let yDiff2 = yDiff * yDiff;
+    let xDiff2 = xDiff * xDiff;
+
+    let distance = Math.sqrt(xDiff2 + yDiff2);
+
+    if (distance < (user.size / 2) + 10) {
+        user.levelUp();
+        newObj();
+    }
+}
+
+io.on('connection', function (socket) {
+    console.log(`user id: ${socket.id} - connected`);
+    addUser(socket.id)
+    console.log(game.currentUsers);
+    io.emit('game', game);
+    socket.on('disconnect', function () {
+        console.log(`user id: ${socket.id} - disconnected`);
+        removeUser(socket.id);
+        io.emit('game', game)
+    });
+    socket.on('chat message', (data) => {
+        console.log(data);
+    });
+    socket.on('move', (data) => {
+        switch (data) {
+            case 'up':
+                getUserById(socket.id).moveUp();
+                break;
+            case 'down':
+                getUserById(socket.id).moveDown();
+                break;
+            case 'right':
+                getUserById(socket.id).moveRight();
+                break;
+            case 'left':
+                getUserById(socket.id).moveLeft();
+                break;
+        }
+        checkUserObj(getUserById(socket.id));
+        io.emit('game', game);
+    })
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
---
+++ public/main.js
@@ -0,0 +1,61 @@
+let getById = (id) => {
+    return document.getElementById(id);
+}
+
+// EMIT EVENT
+socket.emit('chat message', 'test');
+
+socket.on('game', (data) => {
+    console.log(data);
+    let existingUnits = document.getElementsByClassName('unit');
+    let existingObjs = document.getElementsByClassName('objective');
+    if (existingUnits) {
+        while (existingUnits[0]) {
+            existingUnits[0].parentNode.removeChild(existingUnits[0]);
+        }
+    }
+    if (existingObjs) {
+        while (existingObjs[0]) {
+            existingObjs[0].parentNode.removeChild(existingObjs[0]);
+        }
+    }
+    for (unit in data.currentUsers) {
+        let newUnit = document.createElement('DIV');
+        newUnit.className = 'unit';
+        newUnit.style.left = data.currentUsers[unit].left + 'px';
+        newUnit.style.top = data.currentUsers[unit].top + 'px';
+        newUnit.style.width = data.currentUsers[unit].size + 'px';
+        newUnit.style.height = data.currentUsers[unit].size + 'px';
+        newUnit.style.borderRadius = data.currentUsers[unit].size / 2 + 'px';
+        document.getElementsByTagName('body')[0].appendChild(newUnit);
+    }
+    for (obj in data.objectives) {
+        let newObj = document.createElement('DIV');
+        newObj.className = 'objective';
+        newObj.style.left = data.objectives[obj].left + 'px';
+        newObj.style.top = data.objectives[obj].top + 'px';
+        document.getElementsByTagName('body')[0].appendChild(newObj);
+    }
+})
+
+
+getById('moveUp').addEventListener('click', () => moveUp());
+getById('moveDown').addEventListener('click', () => moveDown());
+getById('moveLeft').addEventListener('click', () => moveLeft());
+getById('moveRight').addEventListener('click', () => moveRight());
+
+let moveUp = () => {
+    socket.emit('move', 'up');
+}
+
+let moveDown = () => {
+    socket.emit('move', 'down');
+}
+
+let moveLeft = () => {
+    socket.emit('move', 'left');
+}
+
+let moveRight = () => {
+    socket.emit('move', 'right');
+}
